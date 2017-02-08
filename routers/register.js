241d4525a4c16b66161d71bf6d05c68f81d9b226
wangsidi@baichebao.com <wangsidi@baichebao.com>
2017-02-08 17:01:50 +0800

    完善邮箱登录 及落地页面

--- routers/register.js
+++ routers/register.js
@@ -5,7 +5,7 @@ var nodemailer = require("nodemailer");
 var User = require('../lib/User');
 var jwt = require('koa-jwt');
 
-function sendEmail(key,token){
+function sendEmail(key,email){
   var stmpTransport = nodemailer.createTransport("SMTP",{
     host:"smtp.qq.com",//主机
     secureConnection:true,//使用SSL
@@ -18,9 +18,9 @@ function sendEmail(key,token){
 
   var mailOptions = {
     from:"Fred Foo <272535439@qq.com>",//标题
-    to: "wsd_wangsidi@163.com",//收件人
+    to: email,//收件人
     subject: "Hello world", // 标题
-    html: "<a href='http://localhost:4000/#/verifyemail?verifyKey=" + key + "'>点击进行邮箱验证</a> 世界，你好！" // html 内容
+    html: "<a href='http://http://112.126.78.86/#/verifyemail?verifyKey=" + key + "'>点击进行邮箱验证</a> 世界，你好！" // html 内容
   };
 
   stmpTransport.sendMail(mailOptions,function(error,response){
@@ -47,7 +47,7 @@ exports.post = function * (){
   var user = yield User.addUser(user);
   console.log('user',user._id.toString());
   var token = jwt.sign(user._id.toString(),'wsd',{expiresIn: 24 * 60 * 60  /* 1 days */});
-  sendEmail(body.activeKey,token);
+  sendEmail(body.activeKey,body.email);
   this.body = {
     code:0,
     token:token,
